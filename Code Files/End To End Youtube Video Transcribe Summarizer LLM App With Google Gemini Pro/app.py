bb54531f33cbc5b7c9010758cf1f03987b8b4e16
MHassan8769 <mhassan8769@gmail.com>
2024-10-29 11:43:58 +0500

    YT Projects finalized

--- Code Files/End To End Youtube Video Transcribe Summarizer LLM App With Google Gemini Pro/app.py
+++ Code Files/End To End Youtube Video Transcribe Summarizer LLM App With Google Gemini Pro/app.py
@@ -13,44 +13,68 @@ prompt="""You are Yotube video summarizer. You will be taking the transcript tex
 and summarizing the entire video and providing the important summary in points
 within 250 words. Please provide the summary of the text given here:  """
 
+safe = [
+        {
+            "category": "HARM_CATEGORY_DANGEROUS",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_HARASSMENT",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_HATE_SPEECH",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+            "threshold": "BLOCK_NONE",
+        },
+        {
+            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+            "threshold": "BLOCK_NONE",
+        },
+    ]
 
 ## getting the transcript data from yt videos
 def extract_transcript_details(youtube_video_url):
+    flag = False
     try:
         video_id=youtube_video_url.split("=")[1]
-        
         transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
-
         transcript = ""
         for i in transcript_text:
             transcript += " " + i["text"]
 
-        return transcript
+        return transcript,flag
 
     except Exception as e:
-        raise e
+        flag = True
+        return e,flag
     
 ## getting the summary based on Prompt from Google Gemini Pro
 def generate_gemini_content(transcript_text,prompt):
 
     model=genai.GenerativeModel("gemini-pro")
-    response=model.generate_content(prompt+transcript_text)
+    response=model.generate_content(prompt+transcript_text,safety_settings=safe)
     return response.text
 
 st.title("YouTube Transcript to Detailed Notes Converter")
 youtube_link = st.text_input("Enter YouTube Video Link:")
 
 if youtube_link:
-    video_id = youtube_link.split("=")[1]
-    print(video_id)
+    video_id = youtube_link.split("=")[1].split('&')[0]
     st.image(f"http://img.youtube.com/vi/{video_id}/0.jpg", use_column_width=True)
 
 if st.button("Get Detailed Notes"):
-    transcript_text=extract_transcript_details(youtube_link)
+    transcript_text,flag=extract_transcript_details(youtube_link)
+    if flag:
+        st.markdown("### Detailed Notes:")
+        st.write("Transcription not availaible")
 
-    if transcript_text:
+    elif transcript_text:
         summary=generate_gemini_content(transcript_text,prompt)
-        st.markdown("## Detailed Notes:")
+        st.markdown("### Detailed Notes:")
         st.write(summary)
 
 
